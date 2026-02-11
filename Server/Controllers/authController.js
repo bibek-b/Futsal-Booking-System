49daef8041f516f0d790a995eca378ef8078de4c
Bibek Gadel <vieekgadel49@gmail.com>
2026-02-11 15:16:47 +0545

    get token from func

--- Server/Controllers/authController.js
+++ Server/Controllers/authController.js
@@ -1,23 +1,27 @@
 import User from "../Models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { generateToken } from "../utils/generateToken.js";
 
 const Register = async (req, res) => {
   const body = req.body;
 
   try {
     const hashedPassword = await bcrypt.hash(body.password, 10);
+    
 
     const user = await User.create({
-      userName: body.userName,
+      userName: body.username,
       email: body.email,
       password: hashedPassword,
       phoneNum: body.phoneNum,
     });
-    return res.status(200).json({ message: "User Created Succesfully" });
+    const { token } = generateToken(user);
+
+    return res.status(200).json({ message: "User Created Succesfully", user, token });
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ error: "Email already exists" });
+    return res.status(500).json({ error: error });
   }
 };
 const Login = async (req, res) => {
@@ -31,13 +35,9 @@ const Login = async (req, res) => {
     //check  password is correct
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ error: "Invalid Credentials" });
-    //generate token
-    const token = jwt.sign(
-      { id: user._id, role: user.role },
-      process.env.JWT_SECRET_KEY,
-      { expiresIn: "1d" }
-    );
-    return res.status(200).json({ Token: token });
+    const { token } = generateToken();
+   
+    return res.status(200).json({ user, token });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: "Login Failed" });
