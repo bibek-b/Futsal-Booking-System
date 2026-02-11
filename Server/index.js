60bc193993bbd2c0ec32b70f784ccf772ee48baf
Bibek Gadel <vieekgadel49@gmail.com>
2026-02-11 16:41:30 +0545

    minor change

--- Server/index.js
+++ Server/index.js
@@ -1,45 +1,45 @@
-import express, { json } from 'express';
-import cors from 'cors';
-import authRoutes from './Routes/authRoutes.js';
-import userRoutes from './Routes/userRoutes.js';
-import bookingRoute from './Routes/bookingRoute.js';
-import mongoose from 'mongoose';
-import {Server} from 'socket.io';
-import http from 'http';
-import initSocket from './Socket/socket.js';
+import express, { json } from "express";
+import cors from "cors";
+import authRoutes from "./Routes/authRoutes.js";
+import userRoutes from "./Routes/userRoutes.js";
+import bookingRoute from "./Routes/bookingRoute.js";
+import mongoose from "mongoose";
+import { Server } from "socket.io";
+import http from "http";
+import initSocket from "./Socket/socket.js";
 
 const app = express();
 
 const server = http.createServer(app);
 
-const conn =  mongoose.connect(process.env.MONGODB_URL);
+const conn = mongoose.connect(process.env.MONGODB_URL);
 
-conn && console.log('DB connected successfully!');
+conn && console.log("DB connected successfully!");
 
-const FRONTEND_URL = "https://futsal-booking-system-azure.vercel.app"
-
+const FRONTEND_URL = "https://futsal-booking-system-azure.vercel.app";
 
 app.use(json());
-app.use(cors({
+app.use(
+  cors({
     origin: FRONTEND_URL,
-    credentials: true
-}));
+    credentials: true,
+  }),
+);
 
 //socket
 const io = new Server(server, {
-    cors: {
-        origin: FRONTEND_URL
-    }
+  cors: {
+    origin: FRONTEND_URL,
+    credentials: true,
+  },
 });
 
 initSocket(io);
 
-
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/booking', bookingRoute);
-
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/booking", bookingRoute);
 
 server.listen(5000, () => {
-    console.log('Server is running on port 5000');
+  console.log("Server is running on port 5000");
 });
