a58b2f3f6969d3a39569b326393471d1f8e38643
Bibek Gadel <vieekgadel49@gmail.com>
2026-02-12 16:54:03 +0545

    lockbody scroll func added when loading

---
+++ Client/src/CustomHooks/useLockBodyScroll.js
@@ -0,0 +1,13 @@
+import { useEffect } from "react"
+
+export const useLockBodyScroll = (lock) => {
+    useEffect(() => {
+        if(lock) {
+        document.body.style.overflow = 'hidden'
+    } else {
+        document.body.style.overflow="auto";
+    }
+
+    return () => document.body.style.overflow='auto'
+    },[lock])
+}
